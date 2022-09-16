f2b1b0885eec8b88b8bf50377a3ca36283fbc8cf
TungNT140601 <tungnt14062001@gmail.com>
2022-09-16 10:38:58 +0700

    up code phan *ngFor

--- Learning_Angular/src/app/product/product.component.ts
+++ Learning_Angular/src/app/product/product.component.ts
@@ -6,14 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-  public name = 'Product 1';
-  public price = 20000;
+  // public name = 'Product 1';
+  // public price = 20000;
+  public products = ['Product1', 'Product2', 'Product3','Product4','Product5','Product6',];
   constructor() { }
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
+    // console.log('product = ' + this.product.findIndex(x => x == 'Product2'));
   }
-  public resetName(): void {
-    console.log("resetName func is running");
-    this.name = '';
-  }
+  // public resetName(): void {
+  //   console.log("resetName func is running");
+  //   this.name = '';
+  // }
 }
