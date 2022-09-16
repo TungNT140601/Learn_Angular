46276936702f0d1dec51520832696796b4e2528d
TungNT140601 <tungnt14062001@gmail.com>
2022-09-16 10:27:20 +0700

    up code if condition

--- Learning_Angular/src/app/product/product.component.ts
+++ Learning_Angular/src/app/product/product.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-
+  public name = 'Product 1';
+  public price = 20000;
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  public resetName(): void {
+    console.log("resetName func is running");
+    this.name = '';
+  }
 }
