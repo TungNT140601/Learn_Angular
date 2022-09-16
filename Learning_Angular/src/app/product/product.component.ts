52ce0d0ba366ff6688ec3b18e7e38e548353ad0d
TungNT140601 <tungnt14062001@gmail.com>
2022-09-16 11:21:40 +0700

    add code mgClass

--- Learning_Angular/src/app/product/product.component.ts
+++ Learning_Angular/src/app/product/product.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Product } from '../product';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
@@ -8,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class ProductComponent implements OnInit {
   // public name = 'Product 1';
   // public price = 20000;
-  public products = ['Product1', 'Product2', 'Product3','Product4','Product5','Product6',];
+  // public products = ['Product1', 'Product2', 'Product3','Product4','Product5','Product6',];
+  public products: Product[] = [
+    { name: 'Product1', price: 20000, quantity: 0 },
+    { name: 'Product2', price: 20000, quantity: 20 },
+    { name: 'Product3', price: 20000, quantity: 20 },
+    { name: 'Product4', price: 20000, quantity: 20 },
+    { name: 'Product5', price: 20000, quantity: 20 }
+  ];
   constructor() { }
 
   public ngOnInit(): void {
