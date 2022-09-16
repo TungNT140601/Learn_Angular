558840e139938f3a1663f5ac1ef453eca403bb2f
TungNT140601 <tungnt14062001@gmail.com>
2022-09-16 09:40:49 +0700

    add code

--- Learning_Angular/src/app/app.module.ts
+++ Learning_Angular/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
+import { OrderComponent } from './order/order.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent
+    ProductComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
